390406d6200661ffc8ae8fc66c2e522cd6293914
PSCaveJack <breajack1@gmail.com>
2025-04-16 00:07:36 +0000

    new folders for types and handlers

---
+++ frontend/types/classTypes.ts
@@ -0,0 +1,10 @@
+export interface Course {
+  courseCode: string;
+  courseName: string;
+  description?: string;
+  prerequisites: string[];
+  units: number;
+  department: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
