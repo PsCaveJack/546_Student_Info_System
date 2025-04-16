5f113f403d6a06490b2ac01b6f9590e39667485b
PSCaveJack <breajack1@gmail.com>
2025-04-16 00:13:33 +0000

    get courses function

--- frontend/handlers/classHandlers.ts
+++ frontend/handlers/classHandlers.ts
@@ -2,3 +2,17 @@ import { Course } from "@/types/classTypes";
 
 const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:5000/api";
 
+export const fetchCourses = async (
+  setCourses: React.Dispatch<React.SetStateAction<Course[]>>
+) => {
+  try {
+    // ${API_BASE} was http://localhost:5000/api, changed to this because of the fallback design
+    const res = await fetch(`${API_BASE}/courses`);
+    if (!res.ok) throw new Error("Failed to fetch users");
+
+    const data = await res.json();
+    setCourses(data);
+  } catch (err) {
+    console.error("❌ fetchCourses error:", err);
+  }
+};
